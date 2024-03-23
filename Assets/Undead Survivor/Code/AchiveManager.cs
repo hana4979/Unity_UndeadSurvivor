5c88073498b80e9df76c8f781f13e63a02a824b9
하나 <o3o_s2__@naver.com>
2024-03-23 12:28:59 +0900

    Feat: 업적 달성 로직과 팝업

--- Assets/Undead Survivor/Code/AchiveManager.cs
+++ Assets/Undead Survivor/Code/AchiveManager.cs
@@ -7,15 +7,18 @@ public class AchiveManager : MonoBehaviour
 {
     public GameObject[] lockCharacter;
     public GameObject[] unlockCharacter;
+    public GameObject uiNotice;
 
     enum Achive { unlockPotato, UnlockBean }
     Achive[] achives;
+    WaitForSecondsRealtime wait;
 
     void Awake()
     {
         // Enum.GetValues() : 주어진 열거형의 데이터를 모두 가져오는 함수
         // (Achive[]) : 타입을 명시적으로 지정하여 타입 맞추기
         achives = (Achive[])Enum.GetValues(typeof(Achive));
+        wait = new WaitForSecondsRealtime(5);
 
         if (!PlayerPrefs.HasKey("MyData"))
         {
@@ -32,7 +35,7 @@ public class AchiveManager : MonoBehaviour
         // 업적 데이터와 동일한 이름의 key로 0을 저장
         foreach (Achive achive in achives)
         {
-            PlayerPrefs.SetInt(achive.ToString(), 1); // 0 : 달성X
+            PlayerPrefs.SetInt(achive.ToString(), 0); // 0 : 달성X
             /*
             PlayerPrefs.SetInt("unlockPotato", 0); // 0 : 달성X
             PlayerPrefs.SetInt("UnlockBean", 0);
@@ -47,7 +50,7 @@ public class AchiveManager : MonoBehaviour
 
     void UnlockCharacter()
     {
-        for(int index = 0; index < lockCharacter.Length; index++)
+        for (int index = 0; index < lockCharacter.Length; index++)
         {
             string achiveName = achives[index].ToString(); // 업적 이름 받기
             bool isUnlock = PlayerPrefs.GetInt(achiveName) == 1; // 업적 달성 여부 판단
@@ -56,8 +59,50 @@ public class AchiveManager : MonoBehaviour
         }
     }
 
-    void Update()
+    // 후속 점검을 위해 LateUpdate에서 진행
+    void LateUpdate()
     {
-        
+        // 업적들 확인
+        foreach (Achive achive in achives) {
+            CheckAchive(achive);
+        }
+    }
+
+    void CheckAchive(Achive achive)
+    {
+        bool isAchive = false;
+
+        switch (achive)
+        {
+            case Achive.unlockPotato:
+                isAchive = GameManager.instance.kill >= 10; // 킬 수가 10 이상이면 true
+                break;
+            case Achive.UnlockBean:
+                isAchive = GameManager.instance.gameTime == GameManager.instance.maxGameTime; // 클리어 조건
+                break;
+        }
+
+        if(isAchive && PlayerPrefs.GetInt(achive.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achive.ToString(), 1); // 해당 업적 달성
+
+            // 알림 창의 자식 오브젝트를 순회
+            for(int index=0; index < uiNotice.transform.childCount; index++)
+            {
+                bool isActive = index == (int)achive; // 순번이 맞으면 활성화
+                uiNotice.transform.GetChild(index).gameObject.SetActive(isActive);
+            }
+            StartCoroutine(NoticeRoutine());
+
+        }
+    }
+
+    IEnumerator NoticeRoutine()
+    {
+        uiNotice.SetActive(true);
+
+        yield return wait;
+
+        uiNotice.SetActive(false);
     }
 }
